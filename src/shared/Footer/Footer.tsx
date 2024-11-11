ba9c5706c0ca7fda1e196e22015fc0d5212bb91b
Sai Kaustubh <saikaustubh10@gmail.com>
2024-11-11 16:58:09 +0000

    updated footer

--- src/shared/Footer/Footer.tsx
+++ src/shared/Footer/Footer.tsx
@@ -1,5 +1,6 @@
 // import Logo from "shared/Logo/Logo";
 // import SocialsList1 from "shared/SocialsList1/SocialsList1";
+import logo from "../../assets/images/logo.svg";
 import { CustomLink } from "../../data/types";
 import React from "react";
 
@@ -12,46 +13,12 @@ export interface WidgetFooterMenu {
 const widgetMenus: WidgetFooterMenu[] = [
 	{
 		id: "5",
-		title: "Getting started",
+		title: "Boarding Together",
 		menus: [
-			{ href: "#", label: "Installation" },
-			{ href: "#", label: "Release Notes" },
-			{ href: "#", label: "Upgrade Guide" },
-			{ href: "#", label: "Browser Support" },
-			{ href: "#", label: "Editor Support" },
-		],
-	},
-	{
-		id: "1",
-		title: "Explore",
-		menus: [
-			{ href: "#", label: "Design features" },
-			{ href: "#", label: "Prototyping" },
-			{ href: "#", label: "Design systems" },
-			{ href: "#", label: "Pricing" },
-			{ href: "#", label: "Security" },
-		],
-	},
-	{
-		id: "2",
-		title: "Resources",
-		menus: [
-			{ href: "#", label: "Best practices" },
-			{ href: "#", label: "Support" },
-			{ href: "#", label: "Developers" },
-			{ href: "#", label: "Learn design" },
-			{ href: "#", label: "Releases" },
-		],
-	},
-	{
-		id: "4",
-		title: "Community",
-		menus: [
-			{ href: "#", label: "Discussion Forums" },
-			{ href: "#", label: "Code of Conduct" },
-			{ href: "#", label: "Community Resources" },
-			{ href: "#", label: "Contributing" },
-			{ href: "#", label: "Concurrent Mode" },
+			{ href: "#", label: "About Us" },
+			{ href: "#", label: "Become a Host" },
+			{ href: "#", label: "Work with Us" },
+			{ href: "#", label: "Contact" },
 		],
 	},
 ];
@@ -81,17 +48,27 @@ const Footer: React.FC = () => {
 	};
 
 	return (
-		<div className="nc-Footer relative py-24 lg:py-32 border-t border-neutral-200 dark:border-neutral-700">
-			<div className="container grid grid-cols-2 gap-y-10 gap-x-5 sm:gap-x-8 md:grid-cols-4 lg:grid-cols-5 lg:gap-x-10 ">
-				<div className="grid grid-cols-4 gap-5 col-span-2 md:col-span-4 lg:md:col-span-1 lg:flex lg:flex-col">
-					<div className="col-span-2 md:col-span-1">{/* <Logo /> */}</div>
-					<div className="col-span-2 flex items-center md:col-span-3">
-						{/* <SocialsList1 className="flex items-center space-x-3 lg:space-x-0 lg:flex-col lg:space-y-2.5 lg:items-start" /> */}
+		<>
+			<div className="nc-Footer relative lg:py-12 border-t border-neutral-200 dark:border-neutral-700">
+				<div className="container flex justify-between gap-y-10 gap-x-5 sm:gap-x-8 md:grid-cols-4 lg:grid-cols-5 lg:gap-x-10 ">
+					<div className="grid grid-cols-4 gap-5 col-span-2 md:col-span-4 lg:md:col-span-1 lg:flex lg:flex-col">
+						<div className="col-span-2 md:col-span-1">
+							<img
+								className="flex-shrink-0"
+								width={70}
+								src={logo}
+								alt={"logo"}
+							/>
+						</div>
+						<div className="col-span-2 flex items-center md:col-span-3">
+							{/* <SocialsList1 className="flex items-center space-x-3 lg:space-x-0 lg:flex-col lg:space-y-2.5 lg:items-start" /> */}
+						</div>
 					</div>
+					{widgetMenus.map(renderWidgetMenuItem)}
 				</div>
-				{widgetMenus.map(renderWidgetMenuItem)}
 			</div>
-		</div>
+			<div className="flex justify-center py-2">© Boarding Together</div>
+		</>
 	);
 };
 
